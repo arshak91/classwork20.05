0a277b5a57a2d66fdcf96e7f07a8db9552e2b13c
Arshak Gasparyan <arshakgasparyan1@gmail.com>
2023-05-22 20:28:24 +0400

    init

---
+++ terraform/ec2/instance.tf
@@ -0,0 +1,48 @@
+
+variable "private_key_path" {
+  type    = string
+  default = "../../../../pem/virginia.pem"
+}
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  type    = string
+  default = "ami-007855ac798b5175e"
+}
+variable "name" {
+  type    = string
+  default = "Test"
+}
+
+
+# Create an EC2 instance
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.web.id
+  vpc_security_group_ids = [aws_security_group.web.id]
+  key_name      = "virginia"
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
+    }
+    inline = [
+      "echo \"${self.public_ip}\" >> ../../../../ansible/inventories",
+      # "ansible-playbook -i \"${self.public_ip}\" ../../../ansible/nginx-container.yml"
+    ]
+  }
+  tags = {
+    Name = "${var.name}-Instance"
+  }
+}
+
+output "aws_instance" {
+  value = aws_instance.web
+}
+# "sudo scp -i ./Desktop/pem/virginia.pem ./Desktop/pem/virginia.pem ubuntu@${self.public_dns}:/home/ubuntu"
---
+++ terraform/ec2/variables.tfvars
@@ -0,0 +1,7 @@
+aws_region = "us-east-1"
+instance_type = "t2.micro"
+name = "Terraform11"
+vpc_cidr="10.0.0.0/16"
+subnet_cidr="10.0.1.0/24"
+route_cidr="0.0.0.0/0"
+# private_key_path="./../../virginia.pem"
